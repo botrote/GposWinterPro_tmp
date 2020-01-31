114bcacdf9d70db9b6af83a4eabb8f48983263b3
chj990707 <chj990707@postech.ac.kr>
2020-01-31 14:20:38 +0900

    AI 기초

---
+++ GPOS Winter Project 2019/Assets/Scripts/AI/AI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AI : MonoBehaviour
+{
+    protected Unit body;
+
+    public AI(Unit paramUnit)
+    {
+        body = paramUnit;
+    }
+}
---
+++ GPOS Winter Project 2019/Assets/Scripts/AI/FriendlyMeleeAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendlyMeleeAI : AI
+{
+    public enum Action {Idle, Rally, Pursue, Engage }
+    protected Action curAction;
+    protected GameObject player;
+    public FriendlyMeleeAI(Unit paramUnit) : base(paramUnit)
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (curAction)
+        {
+            default:
+            case Action.Idle:
+                break;
+            case Action.Rally:
+
+                break;
+            case Action.Pursue:
+                break;
+            case Action.Engage:
+                break;
+        }
+    }
+}
