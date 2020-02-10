0e2a1a4b21290dc1ed070f80cf15525f7a031fd5
KZero9 <35094813+KZero9@users.noreply.github.com>
2020-02-10 15:46:59 +0900

    고블린 폭탄병 구현 완료

--- GPOS Winter Project 2019/Assets/Scripts/Core/WaveManager.cs
+++ GPOS Winter Project 2019/Assets/Scripts/Core/WaveManager.cs
@@ -32,6 +32,7 @@ public class WaveManager : MonoBehaviour
             factorymanager.PlaceUnit("SkeletonB", new Vector2(4, 2));
             factorymanager.PlaceUnit("Orc", new Vector2(2,3));
             factorymanager.PlaceUnit("Troll", new Vector2(6,6));
+            factorymanager.PlaceUnit("Goblin", new Vector2(6,4));
             factorymanager.PlaceUnit("Ghost", new Vector2(4, 6));
         }
     }
