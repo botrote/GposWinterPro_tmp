ef6726a9080de1bf93ebd4db9f86b5431afd81df
Bunting_chj <chj990707@naver.com>
2020-02-13 17:58:14 +0900

    플레이어가 스크린 상하좌우 3분의 1 이내에 있도록 카메라워크 변경, 풀숲 배경 추가

--- GPOS Winter Project 2019/Assets/Scripts/Core/CameraManager.cs
+++ GPOS Winter Project 2019/Assets/Scripts/Core/CameraManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     protected Player player;
+    protected Camera cam;
     // Start is called before the first frame update
     private void Awake()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
+        cam = this.GetComponent<Camera>();
     }
     void Start()
     {
@@ -18,6 +20,12 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.GetComponent<Transform>().position = (Vector3)player.position + new Vector3(0,0,-10);
+        Vector2 offset = (Vector2)cam.WorldToScreenPoint(player.position) - new Vector2(cam.pixelWidth / 2 , cam.pixelHeight/2);
+        //Debug.Log("in pixels : " + offset);
+        //Debug.Log("Screentoworldpoint : " + (cam.ScreenToWorldPoint(offset + new Vector2(cam.pixelWidth / 2, cam.pixelHeight / 2)) -gameObject.transform.position));
+        Vector2 CamoffsetinWorld = (cam.ScreenToWorldPoint(offset + new Vector2(cam.pixelWidth / 2, cam.pixelHeight / 2)) - gameObject.transform.position);
+        float moveX = Mathf.Max(0, offset.x - cam.pixelWidth / 6) + Mathf.Min(0, offset.x + cam.pixelWidth / 6);
+        float moveY = Mathf.Max(0, offset.y - cam.pixelHeight / 6) + Mathf.Min(0, offset.y + cam.pixelHeight / 6);
+        gameObject.transform.Translate(cam.ScreenToWorldPoint(new Vector2(moveX, moveY) + new Vector2(cam.pixelWidth / 2, cam.pixelHeight / 2)) - gameObject.transform.position);
     }
 }
