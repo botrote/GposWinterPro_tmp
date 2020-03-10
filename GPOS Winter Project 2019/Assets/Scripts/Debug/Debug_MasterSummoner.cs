9b65a37ea6de5a0d7e8cb3c28eb4e9e2708c9078
botrote <symbol0328@gmail.com>
2020-03-10 23:20:27 +0900

    아무 행동을 하지 않는 디버깅용 유닛 Dummy(friendly, enemy)추가, Master summoner에서 유닛 4종류까지 소환 가능

--- GPOS Winter Project 2019/Assets/Scripts/Debug/Debug_MasterSummoner.cs
+++ GPOS Winter Project 2019/Assets/Scripts/Debug/Debug_MasterSummoner.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+*   MasterSummoner 
+*
+*
+*/
 public class Debug_MasterSummoner : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject product;
+    public GameObject productA;
+    public GameObject productS;
+    public GameObject productD;
+    public GameObject productF;
     private Camera camera;
     void Start()
     {
@@ -15,9 +23,27 @@ public class Debug_MasterSummoner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.D))
+        if(Input.GetKeyDown(KeyCode.A) && productA != null)
         {
-            GameObject createdProduct = Instantiate(product);
+            GameObject createdProduct = Instantiate(productA);
+            createdProduct.transform.position = camera.ScreenToWorldPoint(Input.mousePosition);
+            createdProduct.transform.position += new Vector3 (0, 0, 10);
+        }
+        else if(Input.GetKeyDown(KeyCode.S) && productS != null)
+        {
+            GameObject createdProduct = Instantiate(productS);
+            createdProduct.transform.position = camera.ScreenToWorldPoint(Input.mousePosition);
+            createdProduct.transform.position += new Vector3 (0, 0, 10);
+        }
+        else if(Input.GetKeyDown(KeyCode.D) && productD != null)
+        {
+            GameObject createdProduct = Instantiate(productD);
+            createdProduct.transform.position = camera.ScreenToWorldPoint(Input.mousePosition);
+            createdProduct.transform.position += new Vector3 (0, 0, 10);
+        }
+        else if(Input.GetKeyDown(KeyCode.F) && productF != null)
+        {
+            GameObject createdProduct = Instantiate(productF);
             createdProduct.transform.position = camera.ScreenToWorldPoint(Input.mousePosition);
             createdProduct.transform.position += new Vector3 (0, 0, 10);
         }
