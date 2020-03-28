067ba848673b43808cf441c04a640a6719e501fe
chj990707 <chj990707@postech.ac.kr>
2020-03-28 23:04:57 +0900

    미니맵 도트 구현

---
+++ GPOS Winter Project 2019/Assets/Scripts/UI/MiniMapManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapManager : MonoBehaviour
+{
+    GameObject dot;
+    void Awake()
+    {
+        dot = Resources.Load("Prefabs/dot") as GameObject;
+        Unit.UnitSpawnEvent += CreateDot;
+    }
+
+    void CreateDot(Unit _unit)
+    {
+        GameObject instanceDot = Instantiate(dot);
+        instanceDot.transform.parent = gameObject.transform;
+        instanceDot.GetComponent<MinimapDot>().setUnit(_unit);
+    }
+}
---
+++ GPOS Winter Project 2019/Assets/Scripts/UI/MinimapDot.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapDot : MonoBehaviour
+{
+    public RectTransform rect;
+    public GameObject Boundary;
+    public Unit unitPointing;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rect = gameObject.GetComponent<RectTransform>();
+        Boundary = GameObject.Find("MapManager").transform.Find("Boundary").gameObject;
+    }
+
+    public void setUnit(Unit _unit)
+    {
+        unitPointing = _unit;
+        Color pointColor = new Color();
+        switch (unitPointing.TeamTag) {
+            default:
+            case Unit.Team.Enemy:
+                pointColor = Color.blue;
+                break;
+            case Unit.Team.Building:
+                pointColor = Color.white;
+                break;
+            case Unit.Team.Friendly:
+                pointColor = Color.red;
+                break;
+        }
+        gameObject.GetComponent<Image>().color = pointColor;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (unitPointing != null)
+        {
+            Vector2 UnitPos = unitPointing.transform.position - Boundary.transform.position;
+            rect.localPosition = UnitPos * 5 / 2;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
